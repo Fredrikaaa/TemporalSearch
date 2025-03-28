3e34bf23012f38e056bc0f40f20cbb82e76fbbb7
fredrikaaa <fredrik00@pm.me>
2025-03-28 15:49:32 +0100

    Added synonyms for stitch index

---
+++ src/main/java/com/example/index/AnnotationType.java
@@ -0,0 +1,12 @@
+package com.example.index;
+
+/**
+ * Enum representing the different types of annotations that can be 
+ * associated with text in the stitch index.
+ */
+public enum AnnotationType {
+    DATE,
+    NER,
+    POS,
+    DEPENDENCY
+} 
